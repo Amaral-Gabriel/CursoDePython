1a0c0abcb2dc55e52e917b7cffddddc59812fca6
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2023-12-02 19:08:49 -0300

    Adição do ex006

---
+++ PythonExercícios/ex007.py
@@ -0,0 +1,7 @@
+# Coleta as notas do aluno
+n1 = float(input("Digite a primeira nota:"))
+n2 = float(input("Agora digite a segunda nota:"))
+# Calcula a média
+media = ((n1 + n2)/2)
+
+print(f"A média do aluno é {media}")
