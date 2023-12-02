b3a7c8c90c3cbdb73776bc2b2e72720448df442b
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2023-12-02 18:51:44 -0300

    Adição do ex005

---
+++ PythonExercícios/ex005.py
@@ -0,0 +1,7 @@
+
+# Solicintando um número ao usuário
+n = int(input("Digite um número:"))
+# Descobrindo o antecessor e o sucessor de n
+ant = n - 1
+suc = n + 1
+print(f"O número {n} tem como seu antecessor o número {ant} e o seu sucessor é {suc}!")
