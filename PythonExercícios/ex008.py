a9550fec77c53baf50dec733db4f2f7137a11599
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2023-12-02 19:19:10 -0300

    Adição do ex008

---
+++ PythonExercícios/ex008.py
@@ -0,0 +1,8 @@
+# Coletando a distâcia
+metros = float(input("Digite a distâcia em metros: "))
+
+cent = int(metros*100)
+mili = int(metros*1000)
+
+print(f"{metros} metros equivale a {cent} centímetros ou {mili} milímetros")
+
