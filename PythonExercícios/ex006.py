b1b4f801952e5ac8614cf16ae13c3fa9b560f3d5
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2023-12-02 19:01:37 -0300

    Adição do ex006

---
+++ PythonExercícios/ex006.py
@@ -0,0 +1,11 @@
+# Coleta um número do usário
+n = int(input("Digite um número:"))
+# Calcula o dobro, o triplo e a raiz
+dob = (n*2)
+tri = (n*3)
+raiz = n**(1/2)
+
+print(f'''Dobro= {dob}
+Triplo= {tri}
+Raiz quadrada= {raiz}
+''')
