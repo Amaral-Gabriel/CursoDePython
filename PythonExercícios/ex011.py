14dd491b1a4e74133381028f33474e0c2143747f
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2023-12-03 15:32:57 -0300

    Adição do ex011

--- PythonExercícios/ex011.py
+++ PythonExercícios/ex011.py
@@ -4,4 +4,4 @@ altu = float(input("Qual a altura da sua parede?"))
 # Calcula a area
 area = (larg*altu)
 tin = (area/2)
-print(f"Para pintar {area:.2f} metros de parede, você precisará de {tin:.2f} litros de tinta.")
+print(f"Para pintar {area:.2f} metros quadrados de parede, você precisará de {tin:.2f} litros de tinta.")
