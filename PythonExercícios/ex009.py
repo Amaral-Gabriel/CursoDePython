38957005503f1085284d8a1ebd82054038199306
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2023-12-03 13:06:08 -0300

    Adição do ex009

---
+++ PythonExercícios/ex009.py
@@ -0,0 +1,27 @@
+# Coletando o número
+n = int(input("Digite um número e veja a tabuada: "))
+# Calculando a tabuada
+x1 = (n*1)
+x2 = (n*2)
+x3 = (n*3)
+x4 = (n*4)
+x5 = (n*5)
+x6 = (n*6)
+x7 = (n*7)
+x8 = (n*8)
+x9 = (n*9)
+x10 = (n*10)
+
+print(f'''Aqui está a tabuada de {n}:
+{n} x 1 = {x1}
+{n} x 2 = {x2}
+{n} x 3 = {x3}
+{n} x 4 = {x4}
+{n} x 5 = {x5}
+{n} x 6 = {x6}
+{n} x 7 = {x7}
+{n} x 8 = {x8}
+{n} x 9 = {x9}
+{n} x 10 =  {x10}
+
+''')
