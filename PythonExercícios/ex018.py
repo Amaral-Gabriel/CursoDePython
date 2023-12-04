8c6434aa1e9823061bb64e5b14451f65424285ba
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2023-12-04 15:14:22 -0300

    Adição do ex018

---
+++ PythonExercícios/ex018.py
@@ -0,0 +1,7 @@
+import math
+# Coleta o ângulo
+ang = float(input("Digite um ângulo:"))
+
+print(f"A ângulo de {ang} tem o Seno de {math.sin(math.radians(ang)):.2f}")
+print(f"A ângulo de {ang} tem o Cosseno de {math.cos(math.radians(ang)):.2f}")
+print(f"A ângulo de {ang} tem a Tangente de {math.tan(math.radians(ang)):.2f}")
