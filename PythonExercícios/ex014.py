583da3579c0b8d0fe5f9995be1c91c379a8c38d9
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2023-12-04 10:58:43 -0300

    Adição do ex011

---
+++ PythonExercícios/ex014.py
@@ -0,0 +1,5 @@
+# Coleta a temperatura
+cel = float(input("Digite a temperatura em graus celsius:"))
+# Calcula em Fº
+far = float(cel*1.8+32)
+print(f"A temperatura de {cel}ºC corresponde a {far}ºF!")
