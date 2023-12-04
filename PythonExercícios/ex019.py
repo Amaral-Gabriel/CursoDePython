bfb97dac7fde902f78bc8be96929e91a0a3138d3
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2023-12-04 15:30:58 -0300

    Adição do ex019

---
+++ PythonExercícios/ex019.py
@@ -0,0 +1,12 @@
+import random
+# Coleta o nome dos alunos
+a1 = input("Qual o nome do primeiro aluno?")
+a2 = input("Qual o nome de segundo aluno?")
+a3 = input("Qual o nome do terceiro aluno?")
+a4 = input("Qual o nome do quarto aluno?")
+# Cria uma lista com os alunos
+alunos = [a1, a2, a3, a4]
+
+print(f"Aluno escolhido foi: {random.choice(alunos)}")
+
+
