957ac5a66f7c6fabf1c6dbdc4683172a9f2522d8
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2023-12-05 20:30:46 -0300

    Adição do ex023

---
+++ PythonExercícios/ex023.py
@@ -0,0 +1,12 @@
+# Coleta o número
+num = int(input("Digite um número entre 0 e 9999: "))
+# Calcula os valores
+uni = num // 1 % 10
+dez = num // 10 % 10
+cen = num // 100 % 10
+mil = num // 1000 % 10
+
+print(f"Unidade: {uni}.")
+print(f"Dezena: {dez}.")
+print(f"Centena: {cen}.")
+print(f"Milhar: {mil}.")
