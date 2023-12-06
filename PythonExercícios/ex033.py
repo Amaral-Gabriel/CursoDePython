50dbbafed3bb79dc0dec5e5d4d95184accd3b2e5
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2023-12-06 13:16:45 -0300

    Adição do ex033

---
+++ PythonExercícios/ex033.py
@@ -0,0 +1,10 @@
+# Coleta os números
+n1 = int(input("Primero valor: "))
+n2 = int(input("Segundo valor: "))
+n3 = int(input("Terceiro valor: "))
+
+lista = [n1, n2, n3]
+lista.sort()
+print(f"O menor valor foi {lista[0]} e o maior valor foi {lista[2]}.")
+
+
