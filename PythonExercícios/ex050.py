c948d51c550ff9709a032bdcc37182976a75600e
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2023-12-10 12:04:04 -0300

    Adição do ex050

---
+++ PythonExercícios/ex050.py
@@ -0,0 +1,9 @@
+# Coleta os números
+soma = 0
+cont = 0
+for c in range(1, 7):
+    num = int(input(f"Digite o {c}º número: "))
+    if num % 2 == 0:
+        soma += num
+        cont += 1
+print(f"Você digitou {cont} pares e a soma foi {soma}.")
