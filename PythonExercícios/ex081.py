f52958af90273083da8647c1186a9628f7e76005
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2024-08-27 17:31:20 -0300

    Create ex081.py

---
+++ PythonExercícios/ex081.py
@@ -0,0 +1,21 @@
+resp = True
+lista = []
+
+while resp:
+    num = int(input("Digite um número: "))
+    if not num in lista:
+        lista.append(num)
+    entrada = input("Deseja continuar? (S/N) ")
+    if entrada == "N":
+        resp = False
+    
+lista.sort()
+lista.reverse()
+
+print(f"Você digitou {len(lista)} números")
+print(f"Esse são os números em ordem descrecente: {lista}")
+
+if 5 in lista:
+    print("O número 5 está na lista.")
+else:
+    print("O número 5 não está na lista.")
